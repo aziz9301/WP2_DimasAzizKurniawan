6c05203dbcffa108ba30103fca5042549daf2faf
aziz9301 <suprafit9301@gmail.com>
2023-03-16 19:43:27 +0700

    Menambahkan Contoh1.php

---
+++ Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "<p>Nama Saya Dimas Aziz Kurniawan, saya tinggal di Rawa Buntu
+        Olahraga yang saya Sukai itu Badminton dan Sepakbola</p>";
+    }
+}
